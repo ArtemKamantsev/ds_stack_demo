9e0bfcb92951cb3fdbe491c00c98b977eb396e68
Artem Kamantsev <kamantsev.artem@gmail.com>
2022-07-25 12:13:55 +0300

    feat: enhance tests for python/decorators

--- python_features/decorators/test_decorators.py
+++ python_features/decorators/test_decorators.py
@@ -23,11 +23,23 @@ class TestDecorators(TestCase):
             def __init__(self):
                 self.value = 42
 
-        @replace_type(A)
         class B:
             def __init__(self):
                 self.value = 0
 
-        b = B()  # actually, 'B' type isn't present anymore, this name just refers to type 'A'
+        B = A
+
+        @replace_type(A)
+        class C:
+            def __init__(self):
+                self.value = 1
+
+        b = B()  # actually, 'B' and 'C' types aren't present anymore, these names just refer to type 'A'
+        c = C()
         self.assertEqual(b.value, 42)
         self.assertIsInstance(b, A)
+        self.assertIs(B, A)
+
+        self.assertEqual(c.value, 42)
+        self.assertIsInstance(c, A)
+        self.assertIs(C, A)
