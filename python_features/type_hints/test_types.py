c1755d3bbb4c0a584148ad0e3c238aa7486007a4
Artem Kamantsev <kamantsev.artem@gmail.com>
2022-07-28 12:50:26 +0300

    feat: tests for python/type_hints

---
+++ python_features/type_hints/test_types.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from typing import Literal, TypeVar, Protocol, runtime_checkable
+
+
+class TestTypeHints(TestCase):
+    def test_literal_not_hashable(self) -> None:
+        non_hashable_value = {1: 2}
+        l1 = Literal[non_hashable_value]
+        l2 = Literal[non_hashable_value]
+        with self.assertRaises(TypeError):
+            is_equal: bool = l1 == l2
+
+    def test_runtime_checkable(self) -> None:
+        T = TypeVar('T', bound=str)
+
+        class Proto1(Protocol[T]):
+            def f(self, arg: T) -> T:
+                return arg
+
+        @runtime_checkable
+        class Proto2(Protocol[T]):
+            def f(self, arg: T) -> T:
+                return arg
+
+        class Implementation:
+            def f(self, arg: int) -> int:
+                return arg
+
+        with self.assertRaises(TypeError):
+            isinstance(Implementation(), Proto1)
+        # checks only the presence of the required methods, not their type signatures
+        self.assertTrue(isinstance(Implementation(), Proto2))
