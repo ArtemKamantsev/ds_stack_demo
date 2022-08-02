5f971b05dbd985b6b16a780eedb45fe9922b79de
Artem Kamantsev <kamantsev.artem@gmail.com>
2022-08-02 09:10:26 +0300

    fix: move test for class scope variable to suitable class

--- python_features/compound_statements/test_classes.py
+++ python_features/compound_statements/test_classes.py
@@ -4,6 +4,18 @@ from unittest import TestCase
 
 
 class TestClass(TestCase):
+    def test_class_variable_scope(self) -> None:
+        class A:
+            class_variable = 0
+
+            def f(self_inner) -> None:
+                # The scope of names defined in a class block is limited to the class block;
+                # it does not extend to the code blocks of methods
+                with self.assertRaises(NameError):
+                    variable = class_variable
+
+        A().f()
+
     def test_default_inheritance(self) -> None:
         class A:
             pass
--- test_main.py
+++ test_main.py
@@ -1,16 +1,4 @@
 import unittest
-from unittest import TestCase, expectedFailure, TestSuite
-
-
-class ClassScope(TestCase):
-    class_variable = 0
-
-    def test_class_variable(self) -> None:
-        # The scope of names defined in a class block is limited to the class block;
-        # it does not extend to the code blocks of methods
-        with self.assertRaises(NameError):
-            variable = class_variable
-
 
 if __name__ == '__main__':
     tests = unittest.defaultTestLoader.discover('.')
