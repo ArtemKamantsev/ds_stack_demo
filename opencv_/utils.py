b03d8c4a4b065f1ab1492df7c143f2065493a178
Artem Kamantsev <kamantsev@involve.software>
2023-10-30 22:03:04 +0200

    upd: utils, remove .idea files

--- opencv_/utils.py
+++ opencv_/utils.py
@@ -3,8 +3,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def show(img: np.ndarray) -> None:
+def show(img: np.ndarray, dpi=100, title=None) -> None:
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    plt.figure(dpi=100)
+    plt.figure(dpi=dpi)
     plt.imshow(img_rgb)
+    if title is not None:
+        plt.title(title)
     plt.show()
